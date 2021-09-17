2f0842373e178d6f01deafcc94b476ef9bfe7e06
Vernetzte Kirche <mgeyer@epv.de>
2021-09-17 08:15:25 +0200

    added validation groups

--- src/YamlFormBuilder.php
+++ src/YamlFormBuilder.php
@@ -12,16 +12,161 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Form\FormInterface;
 
 class YamlFormBuilder extends AbstractType
 {
 
     private $definition;
+    //private $options;
+    private $form;
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setRequired('yamlconfig');
+        $resolver->setRequired('use_groups');
 
+        $resolver->setDefaults([
+            'validation_groups' => function (FormInterface $form) {
+                return $this->resolveValidationGroups($form);
+            },
+            'use_groups' => false
+        ]);
+
+    }
+
+    private function resolveValidationGroups($form){
+        $options = $form->getConfig()->getOptions();
+        if(!$options['use_groups']){
+            return ['Default'];
+        }
+
+        $groups = $this->findAllContraintGroups();
+        $this->form = $form;
+        $this->filterGroups($groups);
+
+        return $groups;
+    }
+
+    private function filterGroups(&$groups){
+        foreach($this->definition['conditionalConstraints'] as $key => $conditions){
+            $logic = key($conditions); # and oder or
+            $result = $this->$logic($conditions[$logic]);
+            if(!$result){
+                # Wenn result == true, wird der Constraint nicht gefiltert
+                $groups = array_diff($groups, [$key]);
+            }
+
+        }
+    }
+
+    private function and($conditions){
+        $results = $this->checkConditions($conditions);
+        # Wenn kein false gefunden wird, sind alle wahr = AND
+        if(in_array(false,$results,true) == true ){
+            return false;
+        }
+        return true;
+    }
+
+    private function or($conditions){
+        $results = $this->checkConditions($conditions);
+
+        # Wenn ein true gefunden wird -> wahr = OR
+        if(in_array(true,$results,true) == true ){
+            return true;
+        }
+        return false;
+    }
+
+    private function checkConditions($conditions){
+        $results = [];
+        foreach($conditions as $item){
+            $check = $item['constraint'];
+            $checkvalue = $item['value'];
+            $formvalue = $this->form->get($item['target'])->getData();
+            $results[] = $this->$check($checkvalue,$formvalue);
+        }
+        return $results;
+    }
+
+    /**
+     * Nicht leer
+     *
+     * @param $checkvalue
+     * @param $formvalue
+     * @return bool
+     */
+    private function notblank($checkvalue,$formvalue){
+        if($formvalue != null && $formvalue != ''){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Genau ein Wert
+     * @param $checkvalue
+     * @param $formvalue
+     * @return bool
+     */
+    private function equals($checkvalue,$formvalue){
+        if(is_array($formvalue)){
+            if(in_array($checkvalue, $formvalue)){
+                return true;
+            }
+            return false;
+        }
+
+        if($checkvalue == $formvalue ){
+            return true;
+        }
+        return false;
+    }
+    /**
+     * Größer als
+     * @param $checkvalue
+     * @param $formvalue
+     * @return bool
+     */
+    private function more($checkvalue,$formvalue){
+        if(is_array($formvalue)){
+            return false;
+        }
+        if($formvalue > $checkvalue ){
+            return true;
+        }
+        return false;
+    }
+    /**
+     * Kleiner als
+     * @param $checkvalue
+     * @param $formvalue
+     * @return bool
+     */
+    private function less($checkvalue,$formvalue){
+        if(is_array($formvalue)){
+            return false;
+        }
+        if($formvalue < $checkvalue ){
+            return true;
+        }
+        return false;
+    }
+
+
+    private function findAllContraintGroups(){
+        $groups = [];
+        foreach($this->definition['formfields'] as $field){
+            if(isset($field['options']['constraints']) ){
+                foreach($field['options']['constraints'] as $constraint){
+                    if(isset($constraint['groups'])){
+                        $groups[] = $constraint['groups'];
+                    }
+                }
+            }
+        }
+        return $groups;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
